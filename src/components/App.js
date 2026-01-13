b17af282331727cb1799bade522d244411fa55a0
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-13 10:14:42 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
+  const [users, setUsers] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {users &&
+        users.map((user) => (
+          <div key={user.id}>
+            <h1>{user.title}</h1>
+            <p>{user.body}</p>
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
